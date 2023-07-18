63ea7ada6a080997dcc1a37960b87e80cf4f051f
Daniel Ilnicki <danielgithubb@gmail.com>
2023-07-18 09:21:15 +0200

    set multer package config

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+const multer = require('multer');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 app.get('/', (req, res) => {
   res.json({ message: 'ok' });
