ce43dc75eced4e7d1f8721ba9557a2284b863ee4
Daniel Ilnicki <danielgithubb@gmail.com>
2023-07-18 20:44:18 +0200

    adding style to loading-component style file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
+import LoadingComponent from './components/LoadingComponent/LoadingComponent';
 import UploadComponent from './components/UploadComponent/UploadComponent';
 
 function App() {
   return (
     <>
-      <UploadComponent />
+      <LoadingComponent />
     </>
   );
 }
