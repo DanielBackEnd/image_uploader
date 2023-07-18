ad996f12223e46ad3a31e312fc8d7f329a2adb97
Daniel Ilnicki <danielgithubb@gmail.com>
2023-07-18 09:36:00 +0200

    setting routes, setting controllers, move config multer to utils folder

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-const multer = require('multer');
+const { imageRouter } = require('./routes/imageRoute');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'images');
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname);
-  },
-});
-
-const upload = multer({ storage: storage });
-
-app.get('/', (req, res) => {
-  res.json({ message: 'ok' });
-});
+app.use('/api', imageRouter);
 
 app.listen(3001, () => {
   console.log('App listening on port 3001!');
