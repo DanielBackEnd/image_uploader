c2dc438a707de3324a0ba622fb8a9d401c9c2fde
Daniel Ilnicki <danielgithubb@gmail.com>
2023-07-19 09:22:07 +0200

    built confirm component jsx file

--- frontend/src/components/ConfirmComponent/ConfirmComponent.jsx
+++ frontend/src/components/ConfirmComponent/ConfirmComponent.jsx
@@ -1,9 +1,24 @@
 import style from './ConfirmComponent.module.css';
+import greenIcon from '../../assets/greenicon.png';
 
 const ConfirmComponent = () => {
   return (
     <>
-      <h1>confirm component</h1>
+      <div className={style.container}>
+        <div className={style.confirmMessage}>
+          <img src={greenIcon} alt='green-icon' />
+          <h3>Uploaded Successfully!</h3>
+        </div>
+        <div className={style.preview}>
+          <img src='' alt='uploaded-image-preview' />
+        </div>
+        <div className={style.linkArea}>
+          <p className={style.link}>
+            http://www.example.com/images/asdasdsadasdasdsadasdasdas/asdasdasd
+          </p>
+          <button className={style.copyBtn}>Copy Link</button>
+        </div>
+      </div>
     </>
   );
 };
