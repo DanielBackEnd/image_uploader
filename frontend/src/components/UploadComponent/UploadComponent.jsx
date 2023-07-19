fc96bf0a344f18912442b587aeab7e66bb2e2884
Daniel Ilnicki <danielgithubb@gmail.com>
2023-07-19 12:57:08 +0200

    added drag and drop functionality

--- frontend/src/components/UploadComponent/UploadComponent.jsx
+++ frontend/src/components/UploadComponent/UploadComponent.jsx
@@ -1,14 +1,36 @@
 import style from './UploadComponent.module.css';
 import dragImage from '../../assets/image.svg';
 
-const UploadComponent = props => {
+const UploadComponent = ({ file, setFile, isLoading, setIsLoading }) => {
+  const dropInArea = e => {
+    e.preventDefault();
+    console.log(e.dataTransfer.files[0]);
+    setFile(e.dataTransfer.files[0]);
+  };
+
+  const dragOverArea = e => {
+    e.preventDefault();
+    console.log('drag over area');
+  };
+
+  const dragOutsideArea = e => {
+    e.preventDefault;
+    console.log('drag outside area');
+  };
+
   return (
     <>
       <div className={style.container}>
         <h1 className={style.title}>Upload your image</h1>
         <p className={style.info}>File should be Jpeg, Png,...</p>
         <form>
-          <div className={style.dragArea}>
+          <div
+            className={style.dragArea}
+            id='drag-area'
+            onDrop={dropInArea}
+            onDragOver={dragOverArea}
+            onDragLeave={dragOutsideArea}
+          >
             <img src={dragImage} alt='drag-area-image' />
             Drag & Drop your image here
           </div>
