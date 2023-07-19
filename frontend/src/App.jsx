02dd5542eb46a590160f14b07ca18287cd15c85b
Daniel Ilnicki <danielgithubb@gmail.com>
2023-07-19 09:16:50 +0200

    built basic structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
+import ConfirmComponent from './components/ConfirmComponent/ConfirmComponent';
 import LoadingComponent from './components/LoadingComponent/LoadingComponent';
 import UploadComponent from './components/UploadComponent/UploadComponent';
 
 function App() {
   return (
     <>
-      <LoadingComponent />
+      <ConfirmComponent />
     </>
   );
 }
---
+++ frontend/src/components/ConfirmComponent/ConfirmComponent.jsx
@@ -0,0 +1,11 @@
+import style from './ConfirmComponent.module.css';
+
+const ConfirmComponent = () => {
+  return (
+    <>
+      <h1>confirm component</h1>
+    </>
+  );
+};
+
+export default ConfirmComponent;
