245ae5e351808bdbb35f2cd9ccdb73e4c1868e0d
Daniel Ilnicki <danielgithubb@gmail.com>
2023-07-21 09:53:47 +0200

    replacing standard state with redux-toolkit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,11 @@
-import { useState } from 'react';
 import ConfirmComponent from './components/ConfirmComponent/ConfirmComponent';
 import LoadingComponent from './components/LoadingComponent/LoadingComponent';
 import UploadComponent from './components/UploadComponent/UploadComponent';
 
 function App() {
-  const [file, setFile] = useState();
-  const [isLoading, setIsLoading] = useState(false);
-
   return (
     <>
-      <UploadComponent
-        file={file}
-        setFile={setFile}
-        isLoading={isLoading}
-        setIsLoading={setIsLoading}
-      />
+      <UploadComponent />
     </>
   );
 }
