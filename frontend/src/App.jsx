38110fc3141f74ba2079a2288e173dd2e61c4f16
Daniel Ilnicki <danielgithubb@gmail.com>
2023-07-23 20:33:50 +0200

    set which component should be on page depending on the state

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,22 @@
+import { useSelector } from 'react-redux';
 import ConfirmComponent from './components/ConfirmComponent/ConfirmComponent';
 import LoadingComponent from './components/LoadingComponent/LoadingComponent';
 import UploadComponent from './components/UploadComponent/UploadComponent';
 
 function App() {
+  const file = useSelector(state => state.file.value);
+  const loading = useSelector(state => state.file.loading);
+  const uploaded = useSelector(state => state.file.uploaded);
+
   return (
     <>
-      <UploadComponent />
+      {uploaded ? (
+        <ConfirmComponent />
+      ) : loading ? (
+        <LoadingComponent />
+      ) : (
+        <UploadComponent />
+      )}
     </>
   );
 }
